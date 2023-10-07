573173dafe53ccfd21d5d49c46942dab9b662bd9
jseutens <johan@seutens.eu>
2023-10-07 22:31:27 +0200

    output change

--- includes/shared/output-fields.php
+++ includes/shared/output-fields.php
@@ -23,24 +23,24 @@ function output_html_based_on_field($fields_array) {
                     // OSM https://www.openstreetmap.org/directions?engine=fossgis_osrm_car&route=%3B
 
                     break;
-                case 'aatidlm_phone':
+                case 'aatidlm_dive_type':
                     // Output for phone <a href="tel:123-456-7890">123-456-7890</a>
                     break;
-                case 'aatidlm_cellphone':
+                case 'aatidlm_dive_depth':
                     // Output for cellphone
                     break;
-                case 'aatidlm_fax':
+                case 'aatidlm_dive_level':
                     // Output for fax
                     break;
-                case 'aatidlm_whatsapp':
+                case 'aatidlm_dive_current':
                     // Output for WhatsApp <a href="https://api.whatsapp.com/send?phone=1234567890">Send Message on WhatsApp</a>
 
                     break;
-                case 'aatidlm_e_mail':
+                case 'aatidlm_dive_area':
                     // Output for email <a href="mailto:example@example.com">example@example.com</a>
                     // <a href="mailto: [email protected] "> [email protected] </a>
                     break;
-                case 'aatidlm_ordering_link':
+                case 'aatidlm_dive_visibility':
                     // Output for ordering link <a href="https://www.example.com" target="_blank">Link Text</a> 
                     //    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12">
                     //	<path fill="#36c" d="M6 1h5v5L8.86 3.85 4.7 8 4 7.3l4.15-4.16L6 1ZM2 3h2v1H2v6h6V8h1v2a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1Z"/>
@@ -59,108 +59,13 @@ function output_html_based_on_field($fields_array) {
                       }
                       $output = $aatidlm_link;
                     break;
-                case 'aatidlm_skype':
+                case 'aatidlm_dive_distance':
                     // Output for Skype
                     break;
-                case 'aatidlm_viber':
+                case 'aatidlm_dive_season':
                     // Output for Viber
                     break;
-                case 'aatidlm_telegram':
-                    // Output for Telegram
-                    break;
-                case 'aatidlm_wechat':
-                    // Output for WeChat
-                    break;
-                case 'aatidlm_line':
-                    // Output for Line
-                    break;
-                case 'aatidlm_kakao':
-                    // Output for KakaoTalk
-                    break;
-                case 'aatidlm_weibo':
-                    // Output for Weibo
-                    break;
-                case 'aatidlm_VAT':
-                    // Output for VAT
-                    break;
-                case 'aatidlm_company_id':
-                    // Output for Company ID
-                    break;
-                case 'aatidlm_prof_id':
-                    // Output for Professional ID
-                    break;
-                case 'aatidlm_bank_account':
-                    // Output for Bank account
-                    break;
-                case 'aatidlm_bank_bicswift':
-                    // Output for BIC/SWIFT code
-                    break;
-                case 'aatidlm_facebook':
-                    // Output for Facebook
-                    break;
-                case 'aatidlm_twitter':
-                    // Output for Twitter
-                    break;
-                case 'aatidlm_instagram':
-                    // Output for Instagram
-                    break;
-                case 'aatidlm_youtube':
-                    // Output for YouTube
-                    break;
-                case 'aatidlm_reddit':
-                    // Output for Reddit
-                    break;
-                case 'aatidlm_discord':
-                    // Output for Discord
-                    break;
-                case 'aatidlm_twitch':
-                    // Output for Twitch
-                    break;
-                case 'aatidlm_snapchat':
-                    // Output for Snapchat
-                    break;
-                case 'aatidlm_pinterest':
-                    // Output for Pinterest
-                    break;
-                case 'aatidlm_linkedin':
-                    // Output for LinkedIn
-                    break;
-                case 'aatidlm_tumblr':
-                    // Output for Tumblr
-                    break;
-                case 'aatidlm_flickr':
-                    // Output for Flickr
-                    break;
-                case 'aatidlm_github':
-                    // Output for GitHub
-                    break;
-                case 'aatidlm_bitbucket':
-                    // Output for Bitbucket
-                    break;
-                case 'aatidlm_stackoverflow':
-                    // Output for Stack Overflow
-                    break;
-                case 'aatidlm_medium':
-                    // Output for Medium
-                    break;
-                case 'aatidlm_quora':
-                    // Output for Quora
-                    break;
-                case 'aatidlm_vimeo':
-                    // Output for Vimeo
-                    break;
-                case 'aatidlm_dribbble':
-                    // Output for Dribbble
-                    break;
-                case 'aatidlm_behance':
-                    // Output for Behance
-                    break;
-                case 'aatidlm_deviantart':
-                    // Output for DeviantArt
-                    break;
-                case 'aatidlm_foursquare':
-                    // Output for Foursquare
-                    break;
+
                 default:
                     // Output for any other case
                     break;
--- includes/shared/settings-fields.php
+++ includes/shared/settings-fields.php
@@ -9,10 +9,10 @@ function aatidlm_get_dive_location_data() {
 	$arr_dive_depth=[__('Dive depth :', 'aati-dive-location-manager'),'aatidlm_dive_depth','text',3,'yes',__('The maximum dive depth', 'aati-dive-location-manager')];
 	$arr_dive_level=[__('Diver level :', 'aati-dive-location-manager'),'aatidlm_dive_level','text',4,'yes',__('What type of divers , starter ,advanced, etc', 'aati-dive-location-manager')];
 	$arr_dive_current=[__('Current :', 'aati-dive-location-manager'),'aatidlm_dive_current','text',5,'yes',__('Is the current strong or medium ,etc', 'aati-dive-location-manager')];
-		$arr_dive_area=[__('Area :', 'aati-dive-location-manager'),'aatidlm_dive_area','text',6,'yes',__('Protected marine area ,etc', 'aati-dive-location-manager')];
-		$arr_dive_visibility=[__('Visibility :', 'aati-dive-location-manager'),'aatidlm_dive_visibility','text',7,'yes',__('How far could we possibly see normaly, example 15m-30m ', 'aati-dive-location-manager')];
-		$arr_dive_distance=[__('Time to reach :', 'aati-dive-location-manager'),'aatidlm_dive_distance','text',8,'yes',__('How long does it normaly take to arrive at the destination in min. example 35min', 'aati-dive-location-manager')];
-		$arr_dive_season=[__('Season :', 'aati-dive-location-manager'),'aatidlm_dive_season','text',9,'yes',__('All saesons , only monsoon etc', 'aati-dive-location-manager')];
+	$arr_dive_area=[__('Area :', 'aati-dive-location-manager'),'aatidlm_dive_area','text',6,'yes',__('Protected marine area ,etc', 'aati-dive-location-manager')];
+	$arr_dive_visibility=[__('Visibility :', 'aati-dive-location-manager'),'aatidlm_dive_visibility','text',7,'yes',__('How far could we possibly see normaly, example 15m-30m ', 'aati-dive-location-manager')];
+	$arr_dive_distance=[__('Time to reach :', 'aati-dive-location-manager'),'aatidlm_dive_distance','text',8,'yes',__('How long does it normaly take to arrive at the destination in min. example 35min', 'aati-dive-location-manager')];
+	$arr_dive_season=[__('Season :', 'aati-dive-location-manager'),'aatidlm_dive_season','text',9,'yes',__('All seaons , only monsoon etc', 'aati-dive-location-manager')];
     return [$arr_dive_type, $arr_gps_coords, $arr_dive_depth, $arr_dive_level, $arr_dive_current,$arr_dive_area,$arr_dive_visibility,$arr_dive_distance,$arr_dive_season];
 }
 
